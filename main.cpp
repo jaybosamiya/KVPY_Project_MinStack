64162506eecda312d405e6a44c5df8fdf3fe316b
jaybosamiya <jaybosamiya@gmail.com>
2014-03-12 01:48:16 +0530

    v2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 /*
 
+KVPY Summer Project 2013
+
+Copyright 2013 Jay H. Bosamiya
+
+*/
+
+/*
+
 Miniproject 1 - Linked List Implementation of a Stack with Constant Time Push, Pop, and Findmin
 
-Suppose you are implementing a stack of integers using a singly linked list. The top of the stack 
-will be the first element in the linked list and the bottom of the stack will be the last element 
-in the linked list. It is easy to see that both push and pop operations can be implemented easily 
-in worst case O(1) time (that is, constant time). Suppose now that you want to implement one more 
-operation findmin which will find and return a minimum element in the stack by maintaining an 
-additional linked list which correctly keeps track of the minimum element all the time by 
-suitably updating the links when a push or pop operation is performed. If it is required that 
+Suppose you are implementing a stack of integers using a singly linked list. The top of the stack
+will be the first element in the linked list and the bottom of the stack will be the last element
+in the linked list. It is easy to see that both push and pop operations can be implemented easily
+in worst case O(1) time (that is, constant time). Suppose now that you want to implement one more
+operation findmin which will find and return a minimum element in the stack by maintaining an
+additional linked list which correctly keeps track of the minimum element all the time by
+suitably updating the links when a push or pop operation is performed. If it is required that
 the findmin operations should also have a worst case running time of O(1), then how do you
-implement the additional list, push operation, pop operation, and findmin operation. Note that all 
-three operations must run in worst case constant time. Implement your idea and carry out 
+implement the additional list, push operation, pop operation, and findmin operation. Note that all
+three operations must run in worst case constant time. Implement your idea and carry out
 experimentation on a set of data.
+
+*/
+
+/*
 
 Solution
 Keep two linked lists.
 One of them acts as a normal stack. (call it nStack)
-The other keeps elements in such a way that any element in the stack is not greater than any member below it in the stack. (call it mStack)
+The other keeps elements in such a way that any element in the stack is not greater than any member
+	below it in the stack. (call it mStack)
 
 Push
 Push the element into nStack
@@ -47,18 +60,63 @@ Notes:
 
 #include "MinStack.h"
 
+bool menu();
+
 int main() {
-	int t;
-	MinStack<int> m;
-	m.push(5);
-	m.push(10);
-	m.findMin(t);std::cout << t << std::endl; // 5
-	m.push(2);
-	m.findMin(t);std::cout << t << std::endl; // 2
-	m.pop();
-	m.findMin(t);std::cout << t << std::endl; // 5
-	m.pop();
-	m.pop();
-	m.push(11);
-	m.findMin(t);std::cout << t << std::endl; // 11
+	std::cout <<
+		"MinStack\n"
+		"--------\n\n"
+		"[1] Push element\n"
+		"[2] View top element\n"
+		"[3] Pop top element\n"
+		"[4] Find min element\n"
+		"[0] Exit\n"
+		"\n\n";
+	while(1) {
+		if ( !menu() ) break;
+	}
+}
+
+MinStack<int> ms;
+
+bool menu() {
+	int option, element;
+	std::cout << "Please select an option: ";
+	std::cin >> option;
+	if ( option == 0 ) return false;
+	if ( option < 0 || option > 4 ) {
+		std::cerr << "Error in option selection. Please retry.\n\n";
+		return true;
+	}
+	switch (option) {
+		case 1:
+			std::cout << "Enter element: ";
+			std::cin >> element;
+			ms.push(element);
+			std::cout << "Pushed element " << element << ".\n\n";
+			break;
+		case 2:
+			if ( ms.top(element) ) {
+				std::cout << "Top element is " << element << ".\n\n";
+			} else {
+				std::cerr << "Error: Stack Underflow.\n\n";
+			}
+			break;
+		case 3:
+			if ( ms.top(element) ) {
+				ms.pop();
+				std::cout << "Popped element is " << element << ".\n\n";
+			} else {
+				std::cerr << "Error: Stack Underflow.\n\n";
+			}
+			break;
+		case 4:
+			if ( ms.findMin(element) ) {
+				std::cout << "Min element is " << element << ".\n\n";
+			} else {
+				std::cerr << "Error: Stack Underflow.\n\n";
+			}
+			break;
+	}
+	return true;
 }
